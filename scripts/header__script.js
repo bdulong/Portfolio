00e8025eda3ef38ea96413a758a61144e7e379ab
bdulong <baptistedulong@gmail.com>
2023-12-05 16:34:34 +0100

    nonn

--- scripts/header__script.js
+++ scripts/header__script.js
@@ -5,34 +5,39 @@ $(document).ready(function() {
   var headerHeight = header.outerHeight(); // Hauteur du header
 
   $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop();
 
-      // Soustraire la moitié de la hauteur du header de la position de la section
-      if (scroll >= $(".section__a-propos").offset().top - headerHeight / 3) {
-          header.addClass("transparent");
-          headerLink.css("color", "#0A2463"); // Couleur du texte pour le lien
-          hrElement.css("border-top-color", "#0A2463"); // Couleur de la bordure supérieure hr
-      } else {
-          header.removeClass("transparent");
-          headerLink.css("color", "white"); // Réinitialise la couleur du texte pour le lien
-          hrElement.css("border-top-color", "white"); // Réinitialise la couleur de la bordure supérieure hr
-      }
-  });
+    // Si dans la section projets
+    if (scroll >= $(".section__projets").offset().top - headerHeight / 3 && scroll < $(".section__projets").offset().top + $(".section__projets").height()) {
+        header.css("background-color", "black"); // Change le fond du header en blanc
+        headerLink.css("color", "#0CF25D"); // Couleur du texte pour le lien
+        hrElement.css("border-top-color", "#0CF25D"); // Couleur de la bordure supérieure hr
+    } else {
+        header.css("background-color", "#0A2463"); // Réinitialise la couleur du fond du header
+        headerLink.css("color", "white"); // Réinitialise la couleur du texte pour le lien
+        hrElement.css("border-top-color", "white"); // Réinitialise la couleur de la bordure supérieure hr
+    }
+});
   
     // Ajouter le gestionnaire d'événements pour le style hover
-    headerLink.hover(
-      function() {
-        $(this).css("color", "#2B92DC"); // Couleur du hover
-      },
-      function() {
-        // Réinitialiser la couleur lorsqu'on ne survole plus le lien
-        if ($(window).scrollTop() < $(".section__a-propos").offset().top) {
-          $(this).css("color", "white");
-        } else {
-          $(this).css("color", "#0A2463"); // Couleur du texte pour le lien lorsque scroll est au-dessus de .section__a-propos
-        }
-      }
-    );
+headerLink.hover(
+  function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= $(".section__projets").offset().top - headerHeight / 3 && scroll < $(".section__projets").offset().top + $(".section__projets").height()) {
+      $(this).css("color", "11672F"); // Couleur du hover
+    } else {
+      $(this).css("color", "2B92DC"); // Couleur du hover
+    }
+  },
+  function() {
+    // Réinitialiser la couleur lorsqu'on ne survole plus le lien
+    if ($(window).scrollTop() < $(".section__projets").offset().top || $(window).scrollTop() >= $(".section__projets").offset().top + $(".section__projets").height())  {
+      $(this).css("color", "white");
+    } else {
+      $(this).css("color", "#0CF25D"); // Couleur du texte pour le lien lorsque scroll est au-dessus de .section__projets
+    }
+  }
+);
 
     // Ajouter un gestionnaire d'événements pour le clic sur les sections
     headerLink.on("click", function(event) {
